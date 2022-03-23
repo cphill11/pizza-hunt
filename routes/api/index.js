9444e38fc014ff53983968865e292aec81862079
Criste Phillips <criste.phillips@yahoo.com>
2022-03-23 11:05:34 -0400

    controllers and api routes est

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+// hook the routes set up in pizza-routes.js into the entire server
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
