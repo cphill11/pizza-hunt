51fbee6e29d3aded61ee70716e27ee57ec81378f
Criste Phillips <criste.phillips@yahoo.com>
2022-03-24 14:37:43 -0400

    IndexDB updated

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -8,7 +8,9 @@ request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
   // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts; this will store the db data; autoIncrement improves data retrieval options
-  db.createObjectStore("new_pizza", { autoIncrement: true });
+  db.createObjectStore("new_pizza", {
+    autoIncrement: true,
+  });
 };
 
 // eventhandler for successful connection to db (onsuccess)
@@ -18,8 +20,8 @@ request.onsuccess = function (event) {
 
   // check if app is online, if yes run uploadPizza() function to send all local db data to api; web link: https://developer.mozilla.org/en-US/docs/Web/API/Navigator
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    // commented out prior to 4.7
+    uploadPizza();
   }
 };
 // eventhandler for unsuccessful connection to db (onerror)
@@ -39,3 +41,51 @@ function saveRecord(record) {
   // add record to your store with add method
   pizzaObjectStore.add(record);
 }
+
+// Lines 45+ ADDED AFTER 4.6 (u/a to test code as outlined in 4.6 due to errors; see screenshot)
+// handles collecting all of data from new_pizza object store in IndexedDB and POST it to server
+function uploadPizza() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_pizza"], "readwrite");
+
+  // access your object store
+  const pizzaObjectStore = transaction.objectStore("new_pizza");
+
+  // get all records from store and set to a variable; will have a .result property that is an array of all the data retrieved from new_pizza object store
+  const getAll = pizzaObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function; asyncrhonous function needed to be attached to event handler in order to retrieve data
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/pizzas", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_pizza"], "readwrite");
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore("new_pizza");
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert("All saved pizza has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// browser event listener; listen for app coming back online
+window.addEventListener('online', uploadPizza);
