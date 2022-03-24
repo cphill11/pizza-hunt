5ce0de92d8dda0e91df8b9bba42ac91f1c239c81
Criste Phillips <criste.phillips@yahoo.com>
2022-03-24 14:59:03 -0400

    validators initial

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,49 +1,61 @@
 // import dependencies; Schema constructor & model fxn come straight from Mongoose
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 // create schema w/ desired data after import functionality
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
       // creates timestamp should user not enter a value
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal)
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
-      default: 'Large'
+      required: true,
+      // enum = enumerable (data can be iterated repeatedly, such as for..in loops; provide an array the the size field will accept)
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
+      default: "Large",
     },
     toppings: [],
     comments: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Comment'
-      }
-    ]
+        ref: "Comment",
+      },
+    ],
   },
   // tell schema that it can use virtuals
   {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
-    id: false
-});
+    id: false,
+  }
+);
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
